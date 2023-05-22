d6a28825f7a9ce55b870eee01c34b2326aa4d7fc
maria zamora <maza@marias-Mac-mini.local>
2023-05-22 14:13:30 +0200

    Components added and basic file structure

---
+++ src/components/TodoList.js
@@ -0,0 +1,41 @@
+import React, {useState} from 'react';
+import TodoForm from './TodoForm';
+import Todo from './Todo';
+
+function TodoList() {
+    const [todos, setTodos] = useState([]);
+
+    const addTodo = todo => {
+        if(!todo.text || /^\s*$/.test(todo.text)) {
+            return
+        }
+
+        const newTodos = [todo, ...todos];
+
+        setTodos(newTodos);
+        console.log(...todos);
+    }
+  
+    const completeTodo = id => {
+      let updatedTodos = todos.map(todo => {
+        if (todo.id === id){
+          todo.isComplete = !todo.isComplete;
+        }
+        return todo;
+      });
+      setTodos(updatedTodos);
+    };
+
+  return (
+    <div>
+      <h1>Whats the Plan for Today?</h1>
+      <TodoForm onSubmit={addTodo} />
+      <Todo 
+        todos={todos} 
+        completeTodo={completeTodo} 
+      />
+    </div>
+  );
+}
+
+export default TodoList
